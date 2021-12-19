599fca63d632cd8680831207f03237f2e19cf639
concept10 <tenthconcept@gmail.com>
2021-12-19 14:04:01 -0600

    add npm chalk-animation chalk

--- index.js
+++ index.js
@@ -4,11 +4,15 @@
 
 const { Command } = require('commander');
 const program = new Command();
+const chalkAnimation = require('chald-animation');
 
+chalkAnimation.rainbow('Linix Init - Version 0.1.1');
 program.version('0.0.1');
 
 program
 	.options('-v, --version'), 'Print program version and exit.');
+
+
 
 program.parse(process.argv);
 
--- index.ts
+++ index.ts
@@ -2,3 +2,11 @@
 import { Command } from 'commander';
 const program - new Command();
 
+async function run() { /* code goes here */ }
+
+async function main() {
+  program
+    .command('run')
+    .action(run);
+  await program.parseAsync(process.argv);
+}
